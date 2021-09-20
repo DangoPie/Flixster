8836cb380c1bece302fc87c2e2a38b31870da303
DangoPie <Jessiesu3@gmail.com>
2021-09-20 19:36:55 -0400

    Part 2 of Flixster completed

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,22 +3,32 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
+
 public class Movie {
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
+
+
+    //Empty constructor needed by the Parceler library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
-
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -29,19 +39,32 @@ public class Movie {
         return movies;
     }
 
-    public String getPosterPath() {
+    public double getRating() {
+        return rating;
+    }
+
+    public String getPosterPath()
+    {
         return String.format("https://image.tmdb.org/t/p/original/%s", posterPath);
     }
 
-    public String getBackdropPath() {
+    public String getBackdropPath()
+    {
         return String.format("https://image.tmdb.org/t/p/w1280/%s", backdropPath);
     }
 
-    public String getTitle() {
+    public String getTitle()
+    {
         return title;
     }
 
-    public String getOverview() {
+    public String getOverview()
+    {
         return overview;
+    }
+
+    public int getMovieId()
+    {
+        return movieId;
     }
 }
